f774dc04824d2697faa953b863b864d6449a5be8
Thibault Huyet <thibault.huyet@gmail.com>
2017-10-06 13:03:46 +0100

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,51 @@ use std::path::PathBuf;
 
 fn main()
 	{
-    let mut args: Vec<String> = env::args().collect();
+	    let args: Vec<String> = env::args().collect();
+	    let config = parse_config(&args);
+	    let path = PathBuf::from(&config[0]);
 
-    let len = args.len();
-
-    match len
-    {
-    	1 => 
-    		{
-    			// Considering that I will be using these to make
-    			// paths eventually, maybe doing this is not the best
-    			let path = std::env::current_dir().unwrap();
-    			let path_s = path.to_str().unwrap().to_string();
-    			args.push(path_s);
-    		},
-
-    	2 => println!("Perfect amount of variables"),
-    	_ => panic!("Program only works with 1 or 2 arguments for now"),
-    }
-
-    let path = PathBuf::from(&args[1]);
-
-    for entry in path.read_dir().expect("Read_dir call failed")
-    {
-    	if let Ok(entry) = entry
-    	{
-    		let file = entry.file_name();
-    		println!("{}", file.to_str().unwrap());
-    	}
-    }
+	    for entry in path.read_dir().expect("Read_dir call failed")
+	    {
+	    	if let Ok(entry) = entry
+	    	{
+	    		let file = entry.file_name();
+	    		println!("{}", file.to_str().unwrap());
+	    	}
+	    }
 	}
+
+// Possible inputs should be:
+// Nothing
+// Just a directory
+// A directory and any amount of options
+fn parse_config(args: &[String]) -> Vec<String>
+{
+	let len = args.len();
+	let mut result: Vec<String> = Vec::new();
+
+	match len
+	{
+		// If only 1 input is found, then
+		// nothing was passed to the program
+		// I need to make the default case here
+		// This would be the directory program
+		// was called in
+		1 => {
+				let cur = std::env::current_dir().unwrap();
+				let cur_s = cur.to_str().unwrap().to_string();
+				result.push(cur_s);
+			},
+
+		// If multiple inputs are put in, I
+		// would have to determine whether
+		// It is an option or directory
+		// TO DO: ^^^^^^^ That
+		2 => {
+				let ts = args[1].to_string();
+				result.push(ts);
+			},
+		_ => println!("Please never reach this"),
+	}
+	result
+}
