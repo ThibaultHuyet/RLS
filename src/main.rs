aa48ef66d685d3b45fd59574bb5eb07055a96209
Thibault Huyet <thibault.huyet@gmail.com>
2017-10-06 21:28:59 +0100

    Extracted logic to a seperate file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+extern crate RLS;
+use RLS::conf::*;
 use std::env;
 use std::path::PathBuf;
 
@@ -6,77 +8,4 @@ fn main()
     let args: Vec<String> = env::args().collect();
     let config = parse_config(&args);
     config.print_directory();
-}
-
-
-// config data type
-struct Config
-{
-	path: PathBuf,
-	options: Vec<String>
-}
-
-impl Config
-{
-	pub fn print_directory(&self)
-	{
-		// i is used for formatting
-		let mut i = 0;
-
-		for entry in self.path.read_dir().expect("Could not read")
-		{
-			if let Ok(entry) = entry
-			{
-				let file = entry.file_name();
-				print!("{}  ", file.to_str().unwrap());
-			}
-			// Ensure that only 6 lines are printed on a row
-			i += 1;
-			if i % 6 == 0
-			{
-				println!("");
-			}
-		}
-		println!("");
-	}
-}
-
-// Possible inputs should be:
-// Nothing
-// Just a directory
-// A directory and any amount of options
-fn parse_config(args: &[String]) -> Config
-{
-	let len = args.len();
-	let mut options: Vec<String> = Vec::new();
-	let mut path = PathBuf::new();
-
-	match len
-	{
-		// If only 1 input is found, then
-		// nothing was passed to the program
-		// I need to make the default case here
-		// This would be the directory program
-		// was called in
-		1 => {
-				let cur = std::env::current_dir().unwrap();
-				let cur_s = cur.to_str().unwrap().to_string();
-				path.push(&cur_s);
-			},
-
-		// If multiple inputs are put in, I
-		// would have to determine whether
-		// It is an option or directory
-		// TO DO: ^^^^^^^ That
-		2 => {
-				let ts = args[1].to_string();
-				path.push(&ts);
-			},
-
-		// For now, I'm just testing with 2 inputs
-		// Eventually I will find some way to
-		// 
-		_ => println!("Please never reach this"),
-	}
-	Config{path, options}
 }
